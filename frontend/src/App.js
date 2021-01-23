99dd365d2a80a39df138a51573664901b8a17bcf
zakinao <zakinaororo@gmail.com>
2021-01-23 20:38:28 +0900

    add react

---
+++ frontend/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import {Restaurants} from './containers/Restaurants.jsx';
+import {Foods} from './containers/Foods.jsx';
+import {Orders} from './containers/Orders.jsx'
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        //店舗一覧
+        <Route
+        exact
+        path="/restaurants">
+          <Restaurants/>
+        </Route>
+        //フード一覧
+        <Route
+        exact
+        path="/foods"
+        >
+          <Foods/>
+        </Route>
+        //注文ページ
+        <Route
+        exact
+        path="/orders">
+          <Orders/>
+        </Route>
+
+        <Route
+        exact
+        path="/restaurants/:restaurantsId/foods"
+        render={({match}) => 
+          <Foods
+            match={match}/>
+          }
+        />
+      </Switch>
+    </Router>
+ );
+}
+
+export default App;
---
+++ frontend/src/containers/Restaurants.jsx
@@ -0,0 +1,8 @@
+import React, {Fragment} from 'react';
+export const Restaurants = () => {
+  return(
+    <Fragment>
+      レストラン一覧
+    </Fragment>
+  )
+}
